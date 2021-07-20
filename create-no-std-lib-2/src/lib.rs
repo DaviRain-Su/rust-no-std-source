60730675b84d189ab45c8275eb82b157886774c9
DaviRain-Su <davirain.yin@gamil.com>
2021-07-20 11:51:15 +0800

    Add hello function, and Add test

--- create-no-std-lib-2/src/lib.rs
+++ create-no-std-lib-2/src/lib.rs
@@ -3,11 +3,30 @@
 // 在cargo.toml中的配置使用这个crate的时候，将default-features 设置为false,就是将默认的std环境关闭启动的是no_std
 #![cfg_attr(not(feature = "std"), no_std)]
 
+extern crate alloc;
+use alloc::string::String;
+use sp_std::vec::Vec;
+
+
+fn hello(value: &str) -> Vec<u8> {
+    let tmp  = String::from(value);
+    tmp.into_bytes()
+}
 
 #[cfg(test)]
 mod tests {
+    use crate::hello;
+    use alloc::string::{ToString, String};
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn test_hello() {
+        let left = "hello".to_string();
+        let right = String::from_utf8(hello(left.as_str())).unwrap();
+        assert_eq!(left, right);
     }
 }
